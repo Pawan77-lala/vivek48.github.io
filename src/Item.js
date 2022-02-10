c6d14ba0e7ecdad7a5f2c5fa8915959a95645b67
pawan kumar singh <77200147+Pawan77-lala@users.noreply.github.com>
2022-02-10 23:59:28 +0530

    gdhudh

---
+++ src/Item.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import Modal from './component/Modal';
+
+function Item(props){
+    const [dogImage, setDogImage] = useState(false);
+    const [modal, setModal] = useState(false);
+    const dogName = props.dogs;
+
+    const closeModal = () => {
+        setModal(false)
+    }
+    
+    const getDogImage = () => {
+        fetch(`https://dog.ceo/api/breed/${dogName}/images/random`)
+        .then((response) => response.json())
+        .then((json) => {
+            setDogImage(json.message)
+        })
+    }
+
+    useEffect(() => {
+        getDogImage();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+        <>
+            {modal && <Modal dog={dogName} closeModal={closeModal}/>}
+            <div className="item" onClick={() => setModal(!modal)}>
+                <div className="dog-image">
+                    <img src={dogImage} alt="Dog"/>
+                </div>
+                <p className="dog-name"> {dogName} </p>
+            </div>
+        </>
+    );
+}
+
+export default Item;
